5bd164b3420395a72eee34109cc0153e77bbae32
rafainsunza <rafainsunza@outlook.com>
2024-04-30 14:35:05 +0200

    Display error messages to user

--- static/main.js
+++ static/main.js
@@ -1,12 +1,21 @@
 const form = document.querySelector('form');
 const inputs = form.querySelectorAll('input');
 const submitButton = form.querySelector('.submit-button');
+const invalidMessage = form.querySelector('.invalid-message');
 
 const toggleInvalid = (event) => {
     event.preventDefault();
 
     const input = event.target;
+    const validityState = input.validity;
     const isValid = input.validity.valid;
+
+    if (validityState.valueMissing) {
+        invalidMessage.innerHTML = 'Email address is required'
+    } else if (validityState.typeMismatch) {
+        invalidMessage.innerHTML = 'Please provide a valid email'
+    }
+
 
     input.parentElement.classList.toggle('invalid', !isValid);
 };
