9540a3dd27a8aa11aa2fd31a56cff28714917135
rafainsunza <rafainsunza@outlook.com>
2024-04-30 14:10:07 +0200

    Change validityState() to validity.valid

--- static/main.js
+++ static/main.js
@@ -3,8 +3,12 @@ const inputs = form.querySelectorAll('input');
 const submitButton = form.querySelector('.submit-button');
 
 const toggleInvalid = (event) => {
+    event.preventDefault();
+
     const input = event.target;
-    input.parentElement.classList.toggle('invalid', !input.checkValidity());
+    const isValid = input.validity.valid;
+
+    input.parentElement.classList.toggle('invalid', !isValid);
 };
 
 const handleSubmitClick = () => {
